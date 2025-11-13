5c37325162e64db317989a34ad1217b8aa02fddb
DemileeRachel <demilee462@yahoo.com>
2025-11-13 14:46:07 +0000

    3

--- script/script.js
+++ script/script.js
@@ -1,2 +1,13 @@
 console.log("Game Loaded");
 console.log("JS loaded correctly!");
+function playClick() {
+    const sfx = document.getElementById("sfx-click");
+    sfx.currentTime = 0;
+    sfx.play();
+}
+
+function playSelect() {
+    const sfx = document.getElementById("sfx-select");
+    sfx.currentTime = 0;
+    sfx.play();
+}
